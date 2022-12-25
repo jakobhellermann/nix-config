3bb8f603dcd92df084d273a5cdd53f72ae663c78
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-12-25 23:41:51 +0100

    fix git url

--- .setup/dotfiles.sh
+++ .setup/dotfiles.sh
@@ -6,7 +6,7 @@ GIT_DIR="$HOME/.dotfiles"
 WORK_TREE="$HOME"
 
 rm -f "$HOME/.bashrc"
-git --work-tree "$WORK_TREE" clone --bare git@github.com:jakobhellermann/dotfiles.git "$GIT_DIR"
+git --work-tree "$WORK_TREE" clone --bare https://github.com/jakobhellermann/dotfiles.git "$GIT_DIR"
 git --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" checkout
 
 if [ $? = 0 ]; then
