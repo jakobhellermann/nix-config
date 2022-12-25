8f33850b213834e6d66fb8d122f3c175e1d877cd
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-12-25 23:41:37 +0100

    fixes and packages

--- .setup/arch/pacman.sh
+++ .setup/arch/pacman.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 sudo pacman -Syu
 sudo pacman -S --needed git neovim curl zsh which \
-	ripgrep fd jq tmux htop exa bat ripgrep fzf wget \
+	ripgrep fd jq tmux htop exa bat ripgrep fzf wget xclip \
+	ripgrep fd jq tmux htop exa bat ripgrep fzf wget xclip \
 	graphviz imagemagick \
 	base-devel mold clang shellcheck rustup
 
