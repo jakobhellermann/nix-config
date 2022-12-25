e5641226a1df0df702a80b3a2a9bf2924415e971
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-12-25 23:41:57 +0100

    new gnome settings

--- .setup/gnome.sh
+++ .setup/gnome.sh
@@ -6,6 +6,9 @@ gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 # gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps', 'lv3:lalt_switch', 'apple:badmap']"
 # gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 
+# mouse
+gsettings set org.gnome.desktop.peripherals.mouse speed "-0.65"
+
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
 gsettings set org.gnome.desktop.peripherals.keyboard delay 250
 
@@ -21,6 +24,7 @@ gsettings set org.gnome.desktop.default-applications.terminal exec kitty
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg ""
 
 # style
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,close'
 # gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
 # gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
