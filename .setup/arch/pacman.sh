dd1f308dcd0269ee6a6087b2f4f3d3fd8d146654
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-05-09 13:45:19 +0200

    split gui/multilib, add unzip

--- .setup/arch/pacman.sh
+++ .setup/arch/pacman.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-sudo pacman -Syu
 sudo pacman -S --needed git neovim curl zsh which \
 	ripgrep fd jq sd tmux htop exa bat ripgrep fzf wget xclip skim github-cli \
-	graphviz imagemagick \
+	graphviz imagemagick unzip \
 	base-devel mold clang shellcheck rustup
 
 gui=$(test "$#" -gt 0 && test "$1" = "--gui")
+$gui && sudo pacman -S --needed wl-clipboard noto-fonts firefox kitty ttf-jetbrains-mono obsidian discord signal-desktop
 
-$gui && sudo pacman -S --needed noto-fonts firefox kitty ttf-jetbrains-mono obsidian discord signal-desktop \
-	vulkan-radeon lib32-vulkan-radeon steam
+multilib=$(test "$#" -gt 0 && test "$1" = "--multilib")
+$multilib && sudo pacman -S --needed vulkan-radeon lib32-vulkan-radeon steam
