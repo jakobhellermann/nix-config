8599611d1223223e70bbe0ccd673e1ef17882220
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-12-09 22:07:25 +0100

    homemanager config

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      homeConfigurations = {
+        jakob = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
