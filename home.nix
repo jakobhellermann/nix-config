97933420841d2cb22593723a648e303075cc124a
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-12-11 12:10:10 +0100

    - home system

--- home.nix
+++ home.nix
@@ -14,31 +14,36 @@ in
 
   home.activation = {
     installDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      GIT_BIN=${pkgs.git}/bin/git
       GIT_DIR=".dotfiles"
       WORK_TREE="."
       if [ -d "$GIT_DIR" ]; then
-        $DRY_RUN_CMD git --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" pull || _iError "Could not pull .dotfiles"
+        $DRY_RUN_CMD $GIT_BIN --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" pull || _iError "Could not pull .dotfiles"
       else
-        $DRY_RUN_CMD git --work-tree "$WORK_TREE" clone --bare https://github.com/jakobhellermann/dotfiles.git "$GIT_DIR"
-        $DRY_RUN_CMD git --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" checkout
-        $DRY_RUN_CMD git --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" config status.showUntrackedFiles no
-        $DRY_RUN_CMD git --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" remote set-url origin git@github.com:jakobhellermann/dotfiles.git
+        $DRY_RUN_CMD $GIT_BIN --work-tree "$WORK_TREE" clone --bare https://github.com/jakobhellermann/dotfiles.git "$GIT_DIR"
+        $DRY_RUN_CMD $GIT_BIN --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" checkout
+        $DRY_RUN_CMD $GIT_BIN --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" config status.showUntrackedFiles no
+        $DRY_RUN_CMD $GIT_BIN --git-dir "$GIT_DIR" --work-tree "$WORK_TREE" remote set-url origin git@github.com:jakobhellermann/dotfiles.git
       fi
     '';
   };
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  nixpkgs.config.allowUnfree = true;
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    hello
+    firefox
+    bitwarden-desktop
+    signal-desktop
+    fuzzel
+    waybar
+    nixfmt-rfc-style
+    signal-desktop
+    watchexec
+    just
+    discord
+    spotify
+    vscode
+
     sway-autolayout
 
     # (pkgs.writeShellScriptBin "my-hello" ''
@@ -46,15 +51,8 @@ in
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
@@ -71,16 +69,28 @@ in
     systemd.enable = false;
 
     extraConfig = ''
-      output * bg /home/jakob/Pictures/wallpapers/rotate/a.jpeg fill
+      # output * bg /home/jakob/Pictures/wallpapers/rotate/a.jpeg fill
       set $mod Mod1
 
       include config.d/keybinds/*
       include config.d/*
       include /etc/sway/config.d/*
 
-      exec "${sway-autolayout}/bin/autolayout" > /tmp/autolayout.log
+      # exec "${sway-autolayout}/bin/autolayout" > /tmp/autolayout.log
     '';
   };
 
   programs.home-manager.enable = true;
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+  };
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+
+  services.swayosd.enable = true;
+
+  home.stateVersion = "24.11";
 }
