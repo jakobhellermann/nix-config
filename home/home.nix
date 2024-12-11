aa960bbb425b06fa8dc5b59639b98ff2022cb15c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-12-11 13:16:08 +0100

    modularize home

--- home/home.nix
+++ home/home.nix
@@ -9,6 +9,8 @@ let
   sway-autolayout = inputs.sway-autolayout.packages.${system}.default;
 in
 {
+  nixpkgs.config.allowUnfree = true;
+
   home.username = "jakob";
   home.homeDirectory = "/home/jakob";
 
@@ -28,38 +30,7 @@ in
     '';
   };
 
-  nixpkgs.config.allowUnfree = true;
-
-  home.packages = with pkgs; [
-    firefox
-    bitwarden-desktop
-    signal-desktop
-    fuzzel
-    waybar
-    nixfmt-rfc-style
-    signal-desktop
-    watchexec
-    just
-    discord
-    spotify
-    vscode
-
-    sway-autolayout
-
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  home.file = {
-    # ".screenrc".source = dotfiles/screenrc;
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  home.sessionVariables = { };
+  home.packages = [ sway-autolayout ];
 
   wayland.windowManager.sway = {
     enable = true;
@@ -69,7 +40,6 @@ in
     systemd.enable = false;
 
     extraConfig = ''
-      # output * bg /home/jakob/Pictures/wallpapers/rotate/a.jpeg fill
       set $mod Mod1
 
       include config.d/keybinds/*
@@ -79,18 +49,19 @@ in
       # exec "${sway-autolayout}/bin/autolayout" > /tmp/autolayout.log
     '';
   };
+  services.swayosd.enable = true;
 
   programs.home-manager.enable = true;
-  programs.zsh.oh-my-zsh = {
-    enable = true;
-  };
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-  };
+  # programs.zsh.oh-my-zsh = {
+  #   enable = true;
+  # };
+  # programs.neovim = {
+  #   enable = true;
+  #   vimAlias = true;
+  #   vimdiffAlias = true;
+  # };
 
-  services.swayosd.enable = true;
+  # services.swayosd.enable = true;
 
   home.stateVersion = "24.11";
 }
