3797fa3306345fae871623a8d2b3ddc962e06dc8
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-01-13 23:52:54 +0100

    fish fzf

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -5,3 +5,5 @@ abbr -a fish-reload-config 'source ~/.config/fish/**/*.fish'
 bind -M insert \e\[A 'history-prefix-search-backward'
 bind -M insert \e\[B 'history-prefix-search-forward'
 
+bind \ct -M insert 'commandline -i (fd --hidden --max-depth 4 | fzf | tr -d \n)'
+bind \cw -M insert 'commandline -r; commandline (fd --type d --hidden --max-depth 4 | fzf); commandline -f execute'
--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -1,2 +1,4 @@
 set -g EDITOR nvim
 set -g RUSTUP_TOOLCHAIN nightly
+
+set -xg FZF_DEFAULT_OPTS '--reverse --info hidden'
