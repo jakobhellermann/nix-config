634c64be9841ba560ac73254480320c2357ea7db
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-01-25 17:20:35 +0100

    fish: write ^W as function

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -1,9 +1,17 @@
-# set fish_key_bindings fish_hybrid_key_bindings
- 
 abbr -a fish-reload-config 'source ~/.config/fish/**/*.fish'
  
 bind -M insert \e\[A 'history-prefix-search-backward'
 bind -M insert \e\[B 'history-prefix-search-forward'
 
-bind \cw -M insert 'if set res (fd --hidden --max-depth 5 -E .jj -E .cache -E .npm -E .wine | fzf); test -z (commandline) -a -f "$res" && commandline -i "vim "; commandline -i "$res"; commandline -f execute; end'
-bind \ct -M insert 'echo -n Use ctrl+w'
+function interactive-picker
+	if set res (fd --hidden --max-depth 5 | fzf);
+		set cli (commandline)
+
+		test -z $cli -a -f "$res" && commandline -i "vim ";
+		string match -qr '( $)|^$' $cli || commandline -i " "
+
+		commandline -i "$res"
+		commandline -f execute;
+	end
+end
+bind \cw -M insert interactive-picker
