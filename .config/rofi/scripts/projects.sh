98d2cc10265c6c26d85c2d46d29e0d680ac54894
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-02-01 21:03:23 +0100

    alt-c: switch back to code instead of zed
    +jj

--- .config/rofi/scripts/projects.sh
+++ .config/rofi/scripts/projects.sh
@@ -3,8 +3,8 @@
 cd
 
 if [ $# -ne 0 ]; then
-	coproc ( zeditor "$@" > /dev/null 2>&1 )
+	coproc ( code "$@" > /dev/null 2>&1 )
 	exit 0
 fi
 
-fd --prune '^.git$' dev -utd -x echo {//} | sort
+fd --prune '^.git|.jj$' dev -utd -x echo {//} | sort | uniq
