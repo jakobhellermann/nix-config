7dc3ab12ca4fe68ff903d5bf3ab5f553898b6b47
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-02-12 21:13:10 +0100

    fish completions

---
+++ .config/fish/completions/werk.fish
@@ -0,0 +1,1 @@
+complete --keep-order --exclusive --command werk --arguments "(COMPLETE=fish "'/home/jakob/.cache/rust/release/werk'" -- (commandline --current-process --tokenize --cut-at-cursor) (commandline --current-token))"
