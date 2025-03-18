9effa4cd99cae6f1b35c73c3cf5fc20736bbcc60
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-03-18 08:38:33 +0100

    stuff

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -4,3 +4,6 @@ fish_add_path ~/.nix-profile/bin
 fish_add_path ~/.local/npm/bin
 fish_add_path ~/.bun/bin
 fish_add_path ~/.dotnet/tools
+
+set -gx GOPATH ~/.local/share/go
+fish_add_path $GOPATH/bin
