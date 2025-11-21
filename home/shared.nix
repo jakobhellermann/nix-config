6de920b01eb65d518ac3bf387951d0d682534b91
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-11-21 13:34:26 +0100

    new packages

--- home/shared.nix
+++ home/shared.nix
@@ -29,26 +29,31 @@
     clang
     cmake
     comma
+    dix
     dust
     expect
     eza
     fd
     fzf
     gh
-    nvd
     go
+    helix
     hyperfine
     jujutsu
     just
+    mergiraf
     meson
     mold
     neovim
     ninja
+    nix-tree
     nixd
     nixfmt-rfc-style
+    nvd
     python3
     ripgrep
     rustup
+    tokei
     watchexec
   ];
 
